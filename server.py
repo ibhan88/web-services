641064a6ca32d82d4723d910252fa04dbd9444db
Iris B. Han <irisbhan@gmail.com>
2021-06-15 07:02:42 +0000

    Formatted homepage.

--- server.py
+++ server.py
@@ -15,6 +15,36 @@ def index():
 
     return render_template("index.html")
 
+@app.route('/about')
+def about():
+    """Show informative About page"""
+
+    return render_template("about.html")
+
+@app.route('/services')
+def services():
+    """Show page that lists and describes services offered"""
+
+    return render_template("services.html")
+
+@app.route('/pricing')
+def pricing():
+    """Show pricing tiers page"""
+
+    return render_template("pricing.html")
+
+@app.route('/portfolio')
+def portfolio():
+    """Show portfolio index"""
+
+    return render_template("portfolio.html")
+
+@app.route('/contact')
+def contact():
+    """Show contact page"""
+
+    return render_template("contact.html")
+
 
 
 ################################################
